048cd9ea9dfc08939467c398e81233a6feb99ae2
tconticello <conticello@gmail.com>
2022-07-17 02:26:20 -0300

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -8,7 +8,10 @@ const About = () => (
   <Main meta={<Meta title="Lorem ipsum" description="Lorem ipsum" />}>
     <Content>
       <p>
-        This is Datakimia blog. Will bring different topics related to data.
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
+        recusandae quidem. Quaerat molestiae blanditiis doloremque possimus
+        labore voluptatibus distinctio recusandae autem esse explicabo molestias
+        officia placeat, accusamus aut saepe.
       </p>
       <p>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
